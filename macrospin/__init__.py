cda043ce92e6bea16e7a45920bff53e8305522fe
Colin Jermain <cjermain@gmail.com>
2014-11-20 10:11:51 -0500

    Added helper function for getting correct rotation matrix arrays and an IPython notebook for displaying use

--- macrospin/__init__.py
+++ macrospin/__init__.py
@@ -1,6 +1,16 @@
 """
 Includes helper functions for macrospin
 """
+import numpy as np
+
+def rotation_array(R_function, angles):
+    """ Returns an array of rotation matrixes in correct order based on
+    a rotation function and an array of angles in degrees
+    """
+    rotations = np.empty((len(angles),3,3))
+    for i, angle in enumerate(angles):
+        rotations[i] = R_function(angle)
+    return rotations
 
 def Rx(angle_degrees):
     """ Constructs a rotation matrix around x based on an angle in degrees """
