3d42289c93e2c63d4e271f7ce55a61750474ac9d
Colin Jermain <cjermain@gmail.com>
2015-01-15 12:00:21 -0500

    Added placeholders for YIG material functions

---
+++ macrospin/materials/YIG.py
@@ -0,0 +1,19 @@
+def Ms(T):
+    """ Returns the saturation magnetization as a function 
+    of temperature
+    """
+    pass
+
+
+def Kc1(T):
+    """ Returns the 1st order cubic anisotropy term as a 
+    function of temperature
+    """
+    pass
+
+
+def Kc2(T):
+    """ Returns the 1st order cubic anisotropy term as a 
+    function of temperature
+    """
+    pass
